72de04c13f3048905831f4e9453f79876af98989
Diego <diegoferreira4@hotmail.com>
2020-07-26 21:49:16 -0300

    :tada: Initial commit
    Signed-off-by: Diego <diegoferreira4@hotmail.com>

---
+++ src/main/kotlin/vc/com/diego/school/SchoolApplication.kt
@@ -0,0 +1,11 @@
+package vc.com.diego.school
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchoolApplication>(*args)
+}
